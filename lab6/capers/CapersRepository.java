683eb1edcda4d38953540f422bbe174f355cefb3
nekotsyan <fu0.0c0.0k@gmail.com>
2023-11-18 12:45:23 +0800

    1

--- lab6/capers/CapersRepository.java
+++ lab6/capers/CapersRepository.java
@@ -62,7 +62,9 @@ public class CapersRepository {
      * Also prints out the dog's information using toString().
      */
     public static void makeDog(String name, String breed, int age) {
-        // TODO
+        Dog d = new Dog(name, breed, age);
+        System.out.println(d.toString());
+        d.saveDog();
     }
 
     /**
@@ -72,6 +74,6 @@ public class CapersRepository {
      * @param name String name of the Dog whose birthday we're celebrating.
      */
     public static void celebrateBirthday(String name) {
-        // TODO
+        Dog.fromFile(name).haveBirthday();
     }
 }
--- lab6/capers/Dog.java
+++ lab6/capers/Dog.java
@@ -7,11 +7,10 @@ import static capers.Utils.*;
 /** Represents a dog that can be serialized.
  * @author TODO
 */
-public class Dog { // TODO
+public class Dog implements Serializable{
 
     /** Folder that dogs live in. */
-    static final File DOG_FOLDER = null; // TODO (hint: look at the `join`
-                                         //      function in Utils)
+    static final File DOG_FOLDER = CapersRepository.DOG_FOLDER;
 
     /** Age of dog. */
     private int age;
@@ -39,24 +38,23 @@ public class Dog { // TODO
      * @return Dog read from file
      */
     public static Dog fromFile(String name) {
-        // TODO (hint: look at the Utils file)
-        return null;
+        return readObject(join(DOG_FOLDER, name), Dog.class);
     }
-
     /**
      * Increases a dog's age and celebrates!
      */
     public void haveBirthday() {
         age += 1;
-        System.out.println(toString());
+        System.out.println(this.toString());
         System.out.println("Happy birthday! Woof! Woof!");
+        this.saveDog();
     }
 
     /**
      * Saves a dog to a file for future use.
      */
     public void saveDog() {
-        // TODO (hint: don't forget dog names are unique)
+        writeObject(join(DOG_FOLDER, name),this);
     }
 
     @Override
@@ -65,5 +63,4 @@ public class Dog { // TODO
             "Woof! My name is %s and I am a %s! I am %d years old! Woof!",
             name, breed, age);
     }
-
 }
